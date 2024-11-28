784998fb13c20563db09b7270705061c96f0c27c
Vinicius Climaco <climaco.vinicius@gmail.com>
2024-11-28 05:06:03 -0300

    Add project files.

---
+++ viewer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using viewer.Hubs;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCookiePolicy();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<GridEventsHub>("/hubs/gridevents");
+    endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
+
